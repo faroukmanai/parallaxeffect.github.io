7a56aa3ceeaaddfc7c90b593c0f67e575d6f0e97
FAROUK MANAI <79013591+faroukmanai@users.noreply.github.com>
2023-10-11 13:14:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#background",
+        start: "top top",
+        scrub: 1
+    }
+});
+
+const elem = gsap.utils.toArray('.parallax').forEach(elem => {
+    const depth = elem.dataset.depth;
+    const shifting = -(depth * (elem.offsetHeight*1.1));
+    tl.to(elem, {
+        y: shifting,
+        ease: "none"
+    }, 0)
+    .to('#overlay', {
+        opacity: 0,
+        ease: "none"
+    }, 0)
+})
+
+// tl.fromTo("#content", { y: "-30%" }, { y: "30%" });
+
+
+
